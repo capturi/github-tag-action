0fe78c9c80df644633816fe819ce48616a7a6192
Hans-Henrik Ravn <hhravn@gmail.com>
2019-12-11 10:37:46 +0100

    bump tag even if no new commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+set -e
+set -o xtrace
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
 # config
 default_semvar_bump=${DEFAULT_BUMP:-minor}
-with_v=${WITH_V:-false}
 release_branches=${RELEASE_BRANCHES:-master}
-custom_tag=${CUSTOM_TAG}
+
+repo_fullname=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$repo_fullname.git
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Merge Action"
 
 pre_release="true"
 IFS=',' read -ra branch <<< "$release_branches"
@@ -17,8 +29,7 @@ for b in "${branch[@]}"; do
 done
 echo "pre_release = $pre_release"
 
-# fetch tags
-git fetch --tags
+git fetch origin $BRANCH --tags
 
 # get latest tag
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
@@ -28,9 +39,9 @@ tag_commit=$(git rev-list -n 1 $tag)
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$tag
-    exit 0
+    echo "No new commits since previous tag. Bumping..."
+    # echo ::set-output name=tag::$tag
+    # exit 0
 fi
 
 # if there are none, start tags at 0.0.0
@@ -51,20 +62,9 @@ case "$log" in
     * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
 esac
 
-# prefix with 'v'
-if $with_v
-then
-    new="v$new"
-fi
-
 if $pre_release
 then
     new="$new-${commit:0:7}"
-fi
-
-if [ ! -z $custom_tag ]
-then
-    new="$custom_tag"
 fi
 
 echo $new
@@ -73,25 +73,14 @@ echo $new
 echo ::set-output name=new_tag::$new
 echo ::set-output name=tag::$new
 
-if $pre_release
-then
-    echo "This branch is not a release branch. Skipping the tag creation."
-    exit 0
-fi
-
 # push new tag ref to github
 dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
 full_name=$GITHUB_REPOSITORY
-git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
 
 echo "$dt: **pushing tag $new to repo $full_name"
 
-curl -s -X POST $git_refs_url \
--H "Authorization: token $GITHUB_TOKEN" \
--d @- << EOF
+git tag $new $commit
+git tag latest $commit
 
-{
-  "ref": "refs/tags/$new",
-  "sha": "$commit"
-}
-EOF
+git push origin $new
+git push origin latest
